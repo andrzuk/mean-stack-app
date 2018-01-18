98c4b37942e001072d04efa07df40ac947fbb774
Andrzej <andrzuk@gmail.com>
2018-01-18 09:29:09 +0100

    Protecting token authorization module.

--- routes/token.js
+++ routes/token.js
@@ -4,19 +4,24 @@ module.exports = function(params) {
     var token = {};
     
     token.checkAuth = function(headers, callback) {
-        console.log("Database:",db);
         console.log("Headers:", headers);
         db.collection('users', function (err, collection) {
-            collection.findOne({
-                _id: new ObjectID(headers['user-id'])
-            }, function (err, result) {
-                if (result) {
-                    callback(result.token == headers['x-access-token']);
-                }
-                else {
-                    callback(false);
-                }
-            });
+            console.log(err, collection);
+            if (collection != undefined) {
+                collection.findOne({
+                    _id: new ObjectID(headers['user-id'])
+                }, function (err, result) {
+                    if (result) {
+                        callback(result.token == headers['x-access-token']);
+                    }
+                    else {
+                        callback(false);
+                    }
+                });
+            }
+            else {
+                callback(false);
+            }
         });
     };
 
