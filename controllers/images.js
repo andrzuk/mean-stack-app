0be302c6072adb0ea116fcb7d586933cc8addf90
Andrzej <andrzuk@gmail.com>
2018-01-23 13:45:06 +0100

    Other way of sending post request with file upload.

--- controllers/images.js
+++ controllers/images.js
@@ -29,29 +29,20 @@ angular.module('imagesModule', [])
         $http({
             method: 'POST',
             url: '/images',
+            type: 'post',
             headers: {
-                'Content-Type': 'multipart/form-data',
                 'user-id': window.localStorage.getItem('userId'),
                 'x-access-token': window.localStorage.getItem('authToken') 
             },
-            data: {
-                files: $scope.formData
-            },
-            transformRequest: function (data, headersGetter) {
-                var formData = new FormData();
-                angular.forEach(data, function (value, key) {
-                    formData.append(key, value);
-                });
-                var headers = headersGetter();
-                delete headers['Content-Type'];
-                return formData;
-            }
+            contentType: false,
+            processData: false,
+            data: new FormData($('form#upload-form'))
         })
         .then(function (data) {
             console.log(data);
         });
         
-        $http.post('/images', { data: $scope.formData }, $rootScope.urlConfig).then(function (response) {
+        $http.post('/images', new FormData($('form#upload-form')), $rootScope.urlConfig).then(function (response) {
             console.log(response);
             $scope.getImages();
         });
