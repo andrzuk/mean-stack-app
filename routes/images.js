184b79992cb1377ceab82f8c048f3082a7624711
Andrzej <andrzuk@gmail.com>
2018-01-24 18:25:48 +0100

    Get images by index.

--- routes/images.js
+++ routes/images.js
@@ -125,5 +125,22 @@ module.exports = function(params) {
         });
     });
 
+    router.get('/index/:id', function (req, res, next) {
+        db.collection('images', function (err, collection) {
+            collection.findOne({
+                index: req.params.id
+            }, function (err, result) {
+                var file = process.env.OPENSHIFT_DATA_DIR + result.filename;
+                if (fs.existsSync(file)) {
+                    res.sendFile(file);
+                }
+                else {
+                    console.log('FILE NOT FOUND: ', file);
+                    res.sendFile(__dirname + '/../public/file_not_found.png')
+                }
+            });
+        });
+    });
+
     return router;
 };
